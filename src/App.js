19f5ce7d3aff225fb1a3b8ebca87e5cf1aaec940
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-27 20:02:18 -0400

    converted methods to be more readable, allowing props to be inserted directly into the methods

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import './App.css';
 import {SplitScreen} from "./SplitScreen";
 
-const LeftHandComponent = () => {
-    return <h1 style={{backgroundColor: 'green'}}>Some left content</h1>
+const LeftHandComponent = ({name}) => {
+    return <h1 style={{backgroundColor: 'green'}}>Some left content {name}</h1>
 }
-const RightHandComponent = () => {
-    return <h1 style={{backgroundColor: 'red'}}>Some right content</h1>
+const RightHandComponent = ({message}) => {
+    return <h1 style={{backgroundColor: 'red'}}>Some right content {message}</h1>
 }
 
 function App() {
     return (
         <SplitScreen
-        left={LeftHandComponent}
-        right={RightHandComponent}
-        leftWeight={1}
-        rightWeight={3}
-        />
+            left={LeftHandComponent}
+            right={RightHandComponent}
+        >
+            <LeftHandComponent name="Left component"/>
+            <RightHandComponent message="right component"/>
+        </SplitScreen>
     )
 }
 
--- src/SplitScreen.js
+++ src/SplitScreen.js
@@ -10,17 +10,18 @@ flex: ${props => props.weight}
 `
 
 export const SplitScreen = ({
-                                left: Left,
-                                right: Right,
+
+                                children,
                                 leftWeight = 1,
                                 rightWeight = 1,
 
                             }) => {
+    const [left, right] = children;
     return (
         <Container>
-            <Pane weight={leftWeight}><Left/></Pane>
+            <Pane weight={leftWeight}>{left}</Pane>
 
-            <Pane weight={rightWeight}><Right/></Pane>
+            <Pane weight={rightWeight}>{right}</Pane>
         </Container>
     )
 }
