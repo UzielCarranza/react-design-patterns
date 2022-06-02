9503c8a36458935279e32b5e20eedf8ee6ae904b
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-06-01 21:22:43 -0500

    added styling to background modal

--- src/Modal.js
+++ src/Modal.js
@@ -2,7 +2,16 @@ import {useState} from 'react';
 import styled from 'styled-components';
 
 
-const ModalBackground = styled.div``;
+const ModalBackground = styled.div`
+position: fixed;
+z-index: 1;
+left: 0;
+top: 0;
+width: 100%;
+height: 100%;
+overflow: auto;
+background-color: rgb(0,0,0,0.5);
+`;
 
 const ModalBody = styled.div``;
 
