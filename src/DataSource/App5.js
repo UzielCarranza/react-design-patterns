af6c40d271787f12d9051be7700087d8d00e8ed7
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-06-05 15:05:48 -0500

    created component to get items from local storage

--- src/DataSource/App5.js
+++ src/DataSource/App5.js
@@ -9,11 +9,24 @@ const getServerData = url => async () => {
 
 }
 
+
+const getLocalStorageData = key => () => {
+    return localStorage.getItem(key)
+}
+
+const Text = ({ message }) => <h1>{message}</h1>;
+
+
+
 function App5() {
     return (
         <>
             <DataSource getDataFunc={getServerData('/users/1')} resourceName="user" >
                 <UserInfo></UserInfo>
+            </DataSource>
+
+            <DataSource getDataFunc={getLocalStorageData('message')} resourceName="message">
+                <Text/>
             </DataSource>
         </>
     )
