4d467af24c39581c09055386f72e61ace395550f
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-06-05 14:54:27 -0500

    made component to be more readable by using separationg of concerns

--- src/DataSource/App5.js
+++ src/DataSource/App5.js
@@ -3,15 +3,16 @@ import {ProductInfo} from '../containerComponents/ProductInfo';
 import {DataSource} from './DataSource'
 import axios from 'axios';
 
+const getServerData = url => async () => {
+    const response = await axios.get(url);
+    return response.data;
+
+}
 
 function App5() {
     return (
         <>
-            <DataSource getDataFunc={async () => {
-                const response = await axios.get('/users/1');
-                return response.data;
-
-            }} resourceName="user" >
+            <DataSource getDataFunc={getServerData('/users/1')} resourceName="user" >
                 <UserInfo></UserInfo>
             </DataSource>
         </>
