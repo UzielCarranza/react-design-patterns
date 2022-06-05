6af4c9acec96c58897acaab84df88a190d202f15
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-06-05 14:34:21 -0500

    added component to display product by id

--- src/App4.js
+++ src/App4.js
@@ -1,6 +1,7 @@
 import {UserInfo} from './containerComponents/UserInfo';
 import {ProductInfo} from './containerComponents/ProductInfo';
 import {ResourceLoader} from './ResourceLoader';
+import {ProducInfo} from './containerComponents/ProductInfo'
 
 
 function App4() {
@@ -8,6 +9,9 @@ function App4() {
         <>
             <ResourceLoader resourceUrl="/users/2" resourceName="user">
                 <UserInfo/>
+            </ResourceLoader>
+            <ResourceLoader resourceUrl="/products/12" resourceName="product">
+                <ProductInfo/>
             </ResourceLoader>
         </>
     )
