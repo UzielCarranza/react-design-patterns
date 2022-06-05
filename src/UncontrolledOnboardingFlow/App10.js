fdbaed84e53fb6a546398914875d182ab81406e5
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-06-05 17:57:19 -0500

    added on boarding state to collect user's information on input

--- src/UncontrolledOnboardingFlow/App10.js
+++ src/UncontrolledOnboardingFlow/App10.js
@@ -4,26 +4,29 @@ import {UncontrolledOnboardingFlow} from './UncontrolledOnboardingFlow'
 const StepOne = ({goToNext}) => (
     <>
         <h1>Step 1</h1>
-        <button onClick={goToNext}>Next</button>
+        <button onClick={() => goToNext({age: 1})}>Next</button>
     </>
 )
 const StepTwo = ({goToNext}) => (
     <>
         <h1>Step 2</h1>
-        <button onClick={goToNext}>Next</button>
+        <button onClick={() => goToNext({name: 'test'})}>Next</button>
     </>
 )
 const StepThree = ({goToNext}) => (
     <>
         <h1>Step 3</h1>
 
-        <button onClick={goToNext}>Next</button>
+        <button onClick={() => goToNext({hairColor: 'this is a test'})}>Next</button>
     </>
 )
 
 function App10() {
     return (
-        <UncontrolledOnboardingFlow>
+        <UncontrolledOnboardingFlow onFinish={data => {
+            console.log(data)
+            alert("on boarding complete")
+        }}>
             <StepOne/>
             <StepTwo/>
             <StepThree/>
