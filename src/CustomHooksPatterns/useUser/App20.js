37009d9ae5f3a50035c5a0cec4730ec8f6627d14
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-06-12 22:03:33 -0500

    created custom hook to display 3 different users from the server based on id

--- src/CustomHooksPatterns/useUser/App20.js
+++ src/CustomHooksPatterns/useUser/App20.js
@@ -4,11 +4,16 @@ function App20() {
 
     return (
         <>
+
+            <h1>---------------</h1>
+            <h1>Custom hooks</h1>
             <UserInfo userId="1"/>
 
             <UserInfo userId="3"/>
 
             <UserInfo userId="2"/>
+
+            <h1>---------------</h1>
         </>
     )
 }
