5fce267cb27b09bf78ca90c2b4d3e7d694e38dd8
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-06-26 18:09:54 -0500

    using react hooks to crete a mini movies rating application

--- src/hooks/Hooks.js
+++ src/hooks/Hooks.js
@@ -1,12 +1,37 @@
-import {Deliver} from './Deliver'
-import {FormsCheked} from './FormsCheked'
-function Hooks() {
-    return (
-        <>
-            <Deliver/>
-            <FormsCheked/>
-        </>
-    )
+// import {Deliver} from './Deliver'
+// import {FormsCheked} from './FormsCheked'
+// function Hooks() {
+//     return (
+//         <>
+//             <Deliver/>
+//             <FormsCheked/>
+//         </>
+//     )
+// }
+//
+// export default Hooks
+
+import {BsStarFill} from 'react-icons/bs';
+
+const createArray = (length) => [
+    ...Array(length)
+]
+
+
+function Star({selected = false}) {
+    return <BsStarFill color={selected ? "red" : "grey"}/>
+
 }
 
-export default Hooks
+function StarRating({totalStars}) {
+    return createArray(totalStars).map((n, i) => (
+        <Star key={i}></Star>
+    ))
+}
+
+function Hooks() {
+    return <StarRating totalStars={10}/>
+
+}
+
+export default Hooks;
