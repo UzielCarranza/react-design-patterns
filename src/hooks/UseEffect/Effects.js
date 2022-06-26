229abd4d11350edaa303f891ee0e2848ad5712b5
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-06-26 18:30:02 -0500

    using dependency array to manage the state of the data

--- src/hooks/UseEffect/Effects.js
+++ src/hooks/UseEffect/Effects.js
@@ -3,15 +3,23 @@ import {useState, useEffect} from 'react'
 function Effects() {
 
     const [name, setName] = useState("Gym");
+    const [admin, setAdmin] = useState(false);
 
     useEffect(() => {
 
-    })
+        console.log(`celebreate ${name}`)
+    }, [name])
+
+    useEffect(() => {
+        console.log(`user is ${admin ? admin : "not admin"}`)
+    }, [admin])
 
     return (
         <section>
             <p>congratulations {name}</p>
             <button onClick={() => setName("Will")}>change Winner</button>
+            <p>{admin ? " logged in" : "not logged in"}</p>
+            <button onClick={() => setAdmin(true)}>log in</button>
         </section>
     )
 }
